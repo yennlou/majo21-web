150a0c8428ca427b28488d21f5c6b6fd1807091e
Yenn Lou <yennlou21@gmail.com>
2019-10-17 17:20:53 +1300

    little fix

---
+++ frontend/src/components/Demo/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const Demo = styled.div`
+  height: 240px;
+  width: 240px;
+  background: ${({ theme }) => theme.data.SIDEBAR_FONT}
+`
+
+export default Demo
--- frontend/src/components/Sidebar/index.js
+++ frontend/src/components/Sidebar/index.js
@@ -1,1 +1,41 @@
-export { default } from './Sidebar.js'
+import React from 'react'
+import styled, { css } from 'styled-components'
+import Demo from '../Demo'
+
+const SidebarWrapper = styled.div(({ theme }) => {
+  const { SIDEBAR_BG: bgColor, SIDEBAR_FONT: fontColor } = theme.data
+  return css`
+    width: 400px;
+    height: 100%;
+    background-color: ${bgColor};
+    color: ${fontColor};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `
+})
+
+const TextLogo = styled.span`
+  font-size: 36px;
+  letter-spacing: 4px;
+  padding: 32px;
+`
+
+const Footer = ({ theme }, children) => {
+  return (
+    <div>cddd</div>
+  )
+}
+
+const Sidebar = ({ theme }) => {
+  return (
+    <SidebarWrapper>
+      <TextLogo>
+        MAJO21
+      </TextLogo>
+      <Demo />
+    </SidebarWrapper>
+  )
+}
+
+export default Sidebar
