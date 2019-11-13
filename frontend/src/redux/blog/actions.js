31c6dc866edae1c8ab3a90ed5e8e322385bca860
Yenn Lou <yennlou21@gmail.com>
2019-11-13 17:04:51 +1300

    add toc

--- frontend/src/redux/blog/actions.js
+++ frontend/src/redux/blog/actions.js
@@ -16,14 +16,44 @@ export const fetchCollectionFailure = errMsg => ({
   payload: errMsg
 })
 
+const makeToc = (html) => {
+  // eslint-disable-next-line no-undef
+  const parser = new DOMParser()
+  const doc = parser.parseFromString(`<div>${html}</div>`, 'text/xml')
+
+  let tocList = '<ul>'
+  let prev = 'h2'
+  for (const el of doc.all) {
+    if (el.tagName === 'h2') {
+      if (prev === 'h3') {
+        tocList += '</ul>'
+      }
+      tocList += `<li><a href="#${el.id}">${el.textContent}</a></li>`
+      prev = 'h2'
+    } else if (el.tagName === 'h3') {
+      if (prev === 'h2') {
+        tocList += '<ul>'
+      }
+      tocList += `<li><a href="#${el.id}">${el.textContent}</a></li>`
+      prev = 'h3'
+    }
+  }
+  if (prev === 'h3') tocList += '</ul>'
+  tocList += '</ul><hr>'
+  return tocList
+}
+
 const parseDataToBlog = (data) => {
   const { content, _id: id } = data
   const conv = new showdown.Converter({ metadata: true })
   const html = conv.makeHtml(content)
+
+  const toc = makeToc(html)
   const metadata = conv.getMetadata()
+
   return {
     id,
-    html,
+    html: toc + html,
     ...metadata
   }
 }
